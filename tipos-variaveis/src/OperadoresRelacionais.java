d680a0caf9c5fb02530959dab29539d43321ea14
Dosangelos <angelogasf@gmail.com>
2023-05-31 22:25:21 -0300

    Operadores Relacionais

---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,30 @@
+public class OperadoresRelacionais {
+
+    public static void main(String[] args) {
+        String nomeUm = "ANGELO";
+        String nomeDois = new String("ANGELO");
+        System.out.println(nomeUm == nomeDois);
+
+        System.out.println(nomeUm.equals(nomeDois)); // usar para comparar objeto, usar equals
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        if (numero1 == numero2) {
+            System.out.println("a nossa condição é verdadeira");
+
+        }
+
+        System.out.println("NumeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("NumeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("NumeroUm é maior a numeroDois? " + simNao);
+    }
+
+}
