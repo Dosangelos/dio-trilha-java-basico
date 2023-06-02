7772b4f59ee5aa9f5a1632ce6d5b8dc6fab1b99f
Dosangelos <angelogasf@gmail.com>
2023-06-01 22:34:37 -0300

    teste de soma simples

---
+++ tipos-variaveis/src/SomaSimples.java
@@ -0,0 +1,10 @@
+public class SomaSimples {
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 2;
+        int SomaSimples = n1 + n2;
+        System.out.println("Resultado da soma:" + SomaSimples);
+
+    }
+
+}
